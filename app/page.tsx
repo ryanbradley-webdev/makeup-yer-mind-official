6c24eb4db1fececa34c490de6927c1026c7e94f7
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-07 15:16:38 -0500

    Configure root with color variables and set overall page styling and background.

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-
-    </main>
+    <>
+      <header>Header</header>
+      <main className={styles.main}>
+        <h1>Hello World</h1>
+      </main>
+      <footer>
+        &copy; 2023 Shades of Pink. Designed and developed by Ryan Bradley Web Design.
+      </footer>
+    </>
   )
 }
