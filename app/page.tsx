25e5e4ebbc9b41495a16829d7bcf7af89b8e6b9e
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-22 13:07:41 -0400

    Add subhero about section with temporary image. Change body background image to fixed position.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import styles from './page.module.css'
 import LogoName from '../components/assets/LogoName'
 import HeroBtn from '../components/assets/HeroBtn'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
 
@@ -41,6 +42,34 @@ export default function Home() {
 
         </div>
         
+      </section>
+
+      <section className={styles.about}>
+
+        <Image src='/about-333x500.png' height={500} width={333} alt='' />
+
+        <aside>
+
+          <h4>Hey Bebe!</h4>
+
+          <p>Welcome to my blog! I&apos;m so glad you&apos;re here!</p>
+
+          <p>I absolutely LOVE sharing what I know about makeup and skincare, and I know you&apos;ll find something here that you&apos;ll find useful!</p>
+
+          <p>From tutorials to life-hacks, new looks for you to try, new and exciting products, and everything in-between, I&apos;ll share everything with you! So take a look around, check out some of my blog articles, and visit my community on Instagram!</p>
+
+          <p>Ready to get started??</p>
+
+          <Link href={'/'}>
+
+            <HeroBtn className={styles.heroBtnAlt}>
+              Let&apos;s Do It!!
+            </HeroBtn>
+
+          </Link>
+
+        </aside>
+
       </section>
 
     </main>
