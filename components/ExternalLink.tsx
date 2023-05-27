043248849d3f685ab9140728aee5dd84f9f16411
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-27 18:28:53 -0400

    Revert ExternalLink component to previous prop schema, declaring href explicitly.

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,15 +1,18 @@
 interface ExternalLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
     children: React.ReactNode
+    href: string
 }
 
 export default function ExternalLink({
     children,
+    href,
     ...props
 }: ExternalLinkProps) {
     return (
         <a
             target="_blank"
-            rel="noopener noreferrer" 
+            rel="noopener noreferrer"
+            href={href}
             {...props}
         >
             {children}
