305734074c1f4a19f67e6ec21e89b41681b8833d
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-30 17:53:11 -0400

    Add content to promotion dynamic page.

--- app/promotions/[slug]/page.tsx
+++ app/promotions/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getPromoBySlug } from '@/lib/getPromoBySlug'
-import React from 'react'
+import Image from 'next/image'
+import styles from './page.module.css'
+import ExternalLink from '@/components/ExternalLink'
 
 type Params = {
     params: { slug: string }
@@ -12,7 +14,35 @@ export default async function PromotionBySlug({ params }: Params) {
     return (
         <main>
             
-            {promo?.title}
+            {
+                promo ?
+
+                <section className={styles.promo}>
+                    
+                    {
+                        !promo.active && <h2>This promotion is no longer active.</h2>
+                    }
+
+                    <Image src={promo.image} height={320} width={480} alt='' />
+
+                    <h1>
+                        {promo.title}
+                    </h1>
+
+                    <h3>
+                        {promo.description}
+                    </h3>
+
+                    <p>
+                        To get started, just click <ExternalLink href={promo.link}>this link</ExternalLink>!
+                    </p>
+
+                </section> : 
+
+                <p>
+                    No promotion found.
+                </p>
+            }
 
         </main>
     )
