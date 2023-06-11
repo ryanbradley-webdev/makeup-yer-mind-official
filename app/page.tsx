4edcf3c9de6aa7206ba60f8d0303485a1e6fb043
Ryan Bradley <ryanbradley315@gmail.com>
2023-06-11 12:40:58 -0500

    Add hi-res about photo for desktop.

--- app/page.tsx
+++ app/page.tsx
@@ -62,51 +62,57 @@ export default function Home() {
 
       <section className={styles.about} id='about'>
 
-        <div className={styles.about_hero}>
+        <Image src='/about-lg.png' height={550} width={360} alt='' />
 
-          <Image src='/about.png' height={200} width={200} alt='' />
+        <div>
 
-          <h4>
-            Hey Bebe!
-          </h4>
+          <div className={styles.about_hero}>
+
+            <Image src='/about.png' height={200} width={200} alt='' />
+
+            <h4>
+              Hey Bebe!
+            </h4>
+
+          </div>
+
+          <aside>
+
+            <p>
+              Welcome to the MYM community! I&apos;m so happy you&apos;re here!
+            </p>
+
+            <p>
+              &quot;Makeup Yer Mind&apos; is so much more than just a fun play on words. <strong>It&apos;s a mission.</strong>
+            </p>
+
+            <p>
+              It&apos;s meant to be a place for respite from the weight and negativity of the world for you. A place where you can laugh, have community and truly feel beautiful.
+            </p>
+
+            <p>
+              Makeup is fun, and it can make you feel pretty but if your mind doesn&apos;t truly believe you&apos;re beautiful you&apos;ll never feel it on the outside. Beauty begins with your mindset and radiates out. This is makeup <em>for</em> your mind.
+            </p>
+
+            <p>
+              So please, join in on this crazy little beauty adventure. Think of me as your funny little makeup bestie.
+            </p>
+
+            <p>
+              I&apos;m here to serve you with laughs, positivity and, of course, makeup!
+            </p>
+
+            <Signoff />
+
+          </aside>
 
         </div>
 
-        <aside>
-
-          <p>
-            Welcome to the MYM community! I&apos;m so happy you&apos;re here!
-          </p>
-
-          <p>
-            &quot;Makeup Yer Mind&apos; is so much more than just a fun play on words. <strong>It&apos;s a mission.</strong>
-          </p>
-
-          <p>
-            It&apos;s meant to be a place for respite from the weight and negativity of the world for you. A place where you can laugh, have community and truly feel beautiful.
-          </p>
-
-          <p>
-            Makeup is fun, and it can make you feel pretty but if your mind doesn&apos;t truly believe you&apos;re beautiful you&apos;ll never feel it on the outside. Beauty begins with your mindset and radiates out. This is makeup <em>for</em> your mind.
-          </p>
-
-          <p>
-            So please, join in on this crazy little beauty adventure. Think of me as your funny little makeup bestie.
-          </p>
-
-          <p>
-            I&apos;m here to serve you with laughs, positivity and, of course, makeup!
-          </p>
-
-          <Signoff />
-
-          <Link href='/'>
-            <Button variant='solid'>
-              Let&apos;s Do It!!
-            </Button>
-          </Link>
-
-        </aside>
+        <Link href='/'>
+          <Button variant='solid'>
+            Let&apos;s Do It!!
+          </Button>
+        </Link>
 
       </section>
 
