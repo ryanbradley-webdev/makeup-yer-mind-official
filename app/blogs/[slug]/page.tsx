c29cfd8d2c8153e688cdb237ae05de051297fed0
Ryan Bradley <ryanbradley315@gmail.com>
2023-06-11 17:44:36 -0500

    Add additional social-media sharing links to top of page and update styling for mobile and desktop.

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -86,6 +86,8 @@ export default async function BlogBySlug({ params }: Params) {
                         views={views}
                     />
 
+                    <Share />
+
                 </div>
 
                 <Image src={image} height={337} width={448} alt='' />
