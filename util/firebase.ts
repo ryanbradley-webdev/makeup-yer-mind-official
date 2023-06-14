d95d657968c125cc31fcc462be8c21fc023837ff
Ryan Bradley <ryanbradley315@gmail.com>
2023-06-14 16:04:06 -0500

    Add analytics to project.

--- util/firebase.ts
+++ util/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
+import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -8,9 +9,11 @@ const firebaseConfig = {
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 }
 
 export const app = initializeApp(firebaseConfig)
 export const firestore = getFirestore(app)
 export const storage = getStorage(app)
+export const analytics = app.name && typeof window !== 'undefined' ? getAnalytics(app) : null
